aca4fad3bfc005f231c5a87fbc36de45ba88c80c
jwittneb <>
2023-03-08 18:11:10 -0500

    More skeleton, board printing, monster attributes

--- monster.py
+++ monster.py
@@ -1,8 +1,15 @@
 from card import Card
 
 class Monster(Card):
-  def __init__(self, monsterCost, monsterAttack, monsterDefense, monsterName):
+  def __init__(self, monsterCost, monsterAttack, monsterMaxHP, monsterCurrHP, monsterName):
     self.monsterAttack = monsterAttack
-    self.monsterDefense = monsterDefense
+    self.monsterMaxHP = monsterMaxHP
+    self.monsterCurrHP = monsterCurrHP
+
+    # These are various properties that a monster can have, maybe make this a bit more elegant
+    self.canEvolve = 1
+    self.isEvolved = 0
+    self.hasStorm = 0
+    self.hasRush = 0
     Card.__init__(self, monsterCost, monsterName)
 
